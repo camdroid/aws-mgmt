2411077c768e7bbb9cf425581e6e6645926654a5
Cam <camherringshaw@gmail.com>
2017-04-15 18:21:41 +0000

    Call the get_instances function instead of rewriting it here

--- lambda_ec2_start.py
+++ lambda_ec2_start.py
@@ -1,10 +1,12 @@
 import boto3
 import argparse
+from get_instances import get_instances
 
 ec2 = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
 
 def lambda_handler(event, context):
+    instances = get_instances()
     existing_instance = ec2.Instance(id='i-0e6a7023f20d1bc63')
     res = existing_instance.start()
    
@@ -21,17 +23,11 @@ def lambda_handler(event, context):
 }
 
 
-def get_instances():
-    response = ec2_client.describe_instances()
-    import pdb; pdb.set_trace()
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', default=None)
     args = parser.parse_args()
-    get_instances()
-    
+    lambda_handler({}, {})
 
 
 if __name__ == '__main__':
